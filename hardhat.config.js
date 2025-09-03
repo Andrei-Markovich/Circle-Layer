7fe841f4841a6a84c480b973cd2a30db350d329f
Andrei Markovich <167158287+Andrei-Markovich@users.noreply.github.com>
2025-09-02 23:01:44 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ const { CIRCLE_RPC_URL, PRIVATE_KEY, CHAIN_ID } = process.env;
 module.exports = {
   solidity: "0.8.24",
   networks: {
+    // Имя сети должно совпадать с тем, что используешь в CI: circleLayerTestnet
     circleLayerTestnet: {
       url: CIRCLE_RPC_URL || "",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
