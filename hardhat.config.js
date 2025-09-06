e37a5c14e2a3ea77e2f3420d594a2f05dc667fce
Andrei Markovich <167158287+Andrei-Markovich@users.noreply.github.com>
2025-09-05 21:30:45 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,13 @@ const { CIRCLE_RPC_URL, PRIVATE_KEY, CHAIN_ID } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  // Поддерживаем все встречающиеся pragma
+  solidity: {
+    compilers: [
+      { version: "0.8.28", settings: { optimizer: { enabled: true, runs: 200 } } },
+      { version: "0.8.24", settings: { optimizer: { enabled: true, runs: 200 } } }
+    ]
+  },
   networks: {
     circleLayerTestnet: {
       url: CIRCLE_RPC_URL || "",
